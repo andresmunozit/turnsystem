d86df36f8ad71b68959c0523477f64c98bd36793
Andres Munoz <am@andresmunozit.com>
2020-04-08 00:06:47 -0500

    6: Update filter helper with advanced filters

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -1,26 +1,36 @@
 // This function validates filter string sent in te HTTP request and generates a valid filter object ready to be used by Model.find()
 const filterObject = (model, filterString) => {
     if (!filterString) return {}
-    const filterArray = filterString.split(',')
-
-    // Declaration of filter object
-    const filter = {}
-
-    // Validate filter length
-    const schemaKeys = Object.keys(model.schema.paths)
-    if ( filterArray.length < schemaKeys.length ){
-        // Build filter
-        filterArray.forEach( filterEl => {
-            const [key, value] = filterEl.split(':')
-            filter[key] = value
-        })
-        // Validate filter keys vs Schema keys
-        if ( !Object.keys(filter).every( filterKey => schemaKeys.includes(filterKey)) ){
-            throw {error: 'Invalid filter options.'}
-        } 
-    } else {
-        throw {error: 'Invalid filter options.'}
+    
+    try {
+        const filter = JSON.parse(filterString)
+    } catch {
+        throw {error: 'The provided filter has an invalid format.'}
     }
+
+    const schemaKeys = Object.keys(model.schema.paths)
+
+    // Validate filter keys number
+    if ( Object.keys(filter).length > schemaKeys.length ) throw {error: 'Invalid filter keys.'}
+
+    // Validate that all the filters are included into the schema keys
+    if ( !Object.keys(filter).every( el => schemaKeys.includes(el))) throw new Error({error: 'Invalid filter keys.'})
+    
+    // Transform the JSON input to a valid filter
+    Object.entries(filter).forEach( ([key, value]) => {
+        if (value.match(/\/*\//i)) {
+            filter[key] = eval(value)
+        } else if (value.startsWith('$gte:')){
+            filter[key] = {$gte: value.replace('$gte:','')}
+        } else if (value.startsWith('$gt:')){
+            filter[key] = {$gt: value.replace('$gt:','')}
+        } else if (value.startsWith('$lt:')){
+            filter[key] = {$lt: value.replace('$lt:','')}
+        } else if (value.startsWith('$lte:')){
+            filter[key] = {$lte: value.replace('$lte:','')}
+        }
+    })
+
     return filter
 }
 

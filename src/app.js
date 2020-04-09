1cd9903b78b48dab9efd92c8512a32cd4312ce4d
Andres Munoz <am@andresmunozit.com>
2020-04-09 13:39:32 -0500

    6: Configuration of hbs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const hbs = require('hbs')
 const queueRouter = require('./routers/queue')
 require('./db/mongoose') // Mongoose configuration (connection and parameters)
 
@@ -7,11 +8,18 @@ const app = express()
 const port = process.env.PORT
 const path = require('path') // Used to serve static files
 const publicDirPath = path.join(__dirname, '../public') // Static assets folder
+const viewsPath = path.join(__dirname, '../views') // "views" is the default path for views in hbs, it can be changed here in the future
+const partialsPath = path.join(__dirname, '../views/partials')
 
 // Express configuration
 app.use(express.json())
 app.use(express.static(publicDirPath))
 app.use(queueRouter)
+
+// Hbs (Handlebars Express Plugin) configuration
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Test endpoint
 app.get('/test', (req, res) => {

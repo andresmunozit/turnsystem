8455ea0229f93ade18329b10b0f986c8ced55b84
Andres Munoz <am@andresmunozit.com>
2020-04-08 21:02:09 -0500

    6: Tests suite is implemented for filter helper

---
+++ tests/helpers/filter.test.js
@@ -0,0 +1,46 @@
+const { filterObject } = require('../../src/helpers/filter')
+const Queue = require('../../src/models/queue')
+
+// test methods are included like globals because of Jest
+test('Should return a JSON object (equals filter)', () => {
+    const filter = filterObject( Queue, '{"name":"Loans"}' )
+    expect(filter).toEqual({ name: "Loans" })
+})
+
+test('Should return a JSON object (like filter)', () => {
+    const filter = filterObject( Queue, '{"name":"/oans/"}' )
+    expect(filter).toEqual({ name: /oans/ })
+})
+
+test('Should return a JSON object ($gte filter)', () => {
+    const filter = filterObject( Queue, '{"createdAt":"$gte:2020-04-05"}' )
+    expect(filter).toEqual({
+        createdAt: {
+            $gte: "2020-04-05"
+        }
+    })
+})
+test('Should return a JSON object ($gt filter)', () => {
+    const filter = filterObject( Queue, '{"createdAt":"$gt:2020-04-05"}' )
+    expect(filter).toEqual({
+        createdAt: {
+            $gt: "2020-04-05"
+        }
+    })
+})
+test('Should return a JSON object ($lt filter)', () => {
+    const filter = filterObject( Queue, '{"createdAt":"$lt:2020-04-05"}' )
+    expect(filter).toEqual({
+        createdAt: {
+            $lt: "2020-04-05"
+        }
+    })
+})
+test('Should return a JSON object ($lte filter)', () => {
+    const filter = filterObject( Queue, '{"createdAt":"$lte:2020-04-05"}' )
+    expect(filter).toEqual({
+        createdAt: {
+            $lte: "2020-04-05"
+        }
+    })
+})

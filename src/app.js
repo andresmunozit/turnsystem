cbe7b511522087b95905a486004460cadf10bcb8
Andres Munoz <am@andresmunozit.com>
2020-04-28 22:15:10 -0500

    6: Refactor the code of app.js for improve clarity

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const hbs = require('hbs')
+const path = require('path') // Used to serve static files
 const queueRouter = require('./routers/queue')
 require('./db/mongoose') // Mongoose configuration (connection and parameters)
 
 // Variable definitions
-const app = express()
-const path = require('path') // Used to serve static files
 const publicDirPath = path.join(__dirname, '../public') // Static assets folder
 const viewsPath = path.join(__dirname, '../views') // "views" is the default path for views in hbs, it can be changed here in the future
 const partialsPath = path.join(__dirname, '../views/partials')
 
 // Express configuration
+const app = express()
 app.use(express.json())
 app.use(express.static(publicDirPath))
 app.use(queueRouter)

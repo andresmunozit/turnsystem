8a493358355336e5b827727c0d6fe319be960997
Andres Munoz <am@andresmunozit.com>
2020-05-01 00:43:56 -0500

    6: Minor refactor to seed script

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-require('./mongoose')
+const mongoose = require('mongoose');
+require('./mongoose');
 
-const Queue = require('../models/queue')
+const Queue = require('../models/queue');
 const chalk = require('chalk');
 
 const seedQueues = [
@@ -15,8 +15,8 @@ const seedQueues = [
     { name: 'Bank wires', code: 'BW' },
 ];
 
-Queue.insertMany( seedQueues , (error, documents) => {
-    if( error ) console.log( chalk.red( error ) );
-    if( documents ) console.log( chalk.green( `${documents.length} queues successfully created.`) );
+Queue.insertMany( seedQueues , (err, docs) => {
+    if( err ) console.log( chalk.red( err ) );
+    if( docs ) console.log( chalk.green( `${docs.length} queues successfully created.`) );
     mongoose.connection.close();
 });

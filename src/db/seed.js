254c6679a246a6ef308cda7e23a81f0764ed6e53
Andres Munoz <am@andresmunozit.com>
2020-05-05 21:34:01 -0500

    7: Added users to seed script

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-require('./mongoose');
-
-const Queue = require('../models/queue');
 const chalk = require('chalk');
+
+require('./mongoose');
+const Queue = require('../models/queue');
+const User = require('../models/user')
 
 const seedQueues = [
     { name: 'Insurances', code: 'IN' },
@@ -18,5 +19,38 @@ const seedQueues = [
 Queue.insertMany( seedQueues , (err, docs) => {
     if( err ) console.log( chalk.red( err ) );
     if( docs ) console.log( chalk.green( `${docs.length} queues successfully created.`) );
+    mongoose.connection.close();
+});
+
+const seedUsers = [
+    {
+        email: 'Lottie.Koss@yahoo.com',
+        name: 'Deondre',
+        lastname: 'Hansen',
+        idCard: '19833',
+    },
+    {
+        email: 'Kayley16@yahoo.com',
+        name: 'Andy',
+        lastname: 'Brown',
+        idCard: '46367',
+    },
+    {
+        email: 'Giovanny87@hotmail.com',
+        name: 'Arely',
+        lastname: 'Medhurst',
+        idCard: '80017',
+    },
+    {
+        email: 'Lilliana33@hotmail.com',
+        name: 'Daija',
+        lastname: 'Brakus',
+        idCard: '56031',
+    },
+];
+
+User.insertMany( seedUsers, (err, docs) => {
+    if(err) return console.log(chalk.red(err));
+    if(docs) return console.log(chalk.green(`${docs.length} users successfully created`))
     mongoose.connection.close();
 });

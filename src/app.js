9b39aa4cf88fd49148e4212939ef60e13f8dd3ba
Andres Munoz <am@andresmunozit.com>
2020-05-05 21:08:27 -0500

    7: Add support for the user router

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const hbs = require('hbs')
-const path = require('path') // Used to serve static files
-const queueRouter = require('./routers/queue')
-require('./db/mongoose') // Mongoose configuration (connection and parameters)
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path'); // Used to serve static files
+const queueRouter = require('./routers/queue');
+const userRouter = require('./routers/user');
+require('./db/mongoose'); // Mongoose configuration (connection and parameters)
 
 // Variable definitions
-const publicDirPath = path.join(__dirname, '../public') // Static assets folder
-const viewsPath = path.join(__dirname, '../views') // "views" is the default path for views in hbs, it can be changed here in the future
-const partialsPath = path.join(__dirname, '../views/partials')
+const publicDirPath = path.join(__dirname, '../public'); // Static assets folder
+const viewsPath = path.join(__dirname, '../views'); // "views" is the default path for views in hbs, it can be changed here in the future
+const partialsPath = path.join(__dirname, '../views/partials');
 
 // Express configuration
-const app = express()
-app.use(express.json())
-app.use(express.static(publicDirPath))
-app.use(queueRouter)
+const app = express();
+app.use(express.json());
+app.use(express.static(publicDirPath));
+app.use(queueRouter);
+app.use(userRouter);
 
 // Hbs (Handlebars Express Plugin) configuration
 app.set('view engine', 'hbs')

8071b03cd7fbf5512ba1cd9180f84896be4612e4
Andres Munoz <am@andresmunozit.com>
2020-05-10 20:01:11 -0500

    7: Updated formatQuery helper to handle limit and skip

--- src/helpers/formatQuery.js
+++ src/helpers/formatQuery.js
@@ -9,6 +9,25 @@ const formatFilter = filter => {
     return filter;
 };
 
+const formatLimit = limit => {
+    if(!limit) return 0;
+
+    limit = parseInt(limit);
+    if(!limit) return {error: 'Limit must be an integer'};
+
+    return limit;
+};
+
+const formatSkip = skip => {
+    if(!skip) return 0;
+
+    skip = parseInt(skip);
+    if(!skip) return {error: 'Skip must be an integer'};
+    if(skip < 0) return {error: 'Skip value must be a positive integer'};
+
+    return skip;
+};
+
 const formatQuery = query => {
 
     const sort = query.sort;
@@ -16,7 +35,13 @@ const formatQuery = query => {
     const filter = formatFilter(query.filter);
     if(filter.error) return {error: filter.error};
 
-    return {sort, filter};
+    const limit = formatLimit(query.limit);
+    if(limit.error) return {error: limit.error};
+
+    const skip = formatSkip(query.skip)
+    if(skip.error) return {error: skip.error};
+
+    return {sort, filter, limit, skip};
 };
 
 module.exports = { formatQuery };
